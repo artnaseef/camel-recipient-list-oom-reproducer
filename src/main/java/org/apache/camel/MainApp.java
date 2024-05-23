c5e09449a946a573d48228deafa7c72f9ae7381f
Arthur Naseef <art@artnaseef.com>
2024-05-23 10:01:16 -0700

    Camel 2.24.0

--- src/main/java/org/apache/camel/MainApp.java
+++ src/main/java/org/apache/camel/MainApp.java
@@ -12,7 +12,8 @@ public class MainApp {
      */
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        main.configure().addRoutesBuilder(new MyRouteBuilder());
+        CamelContext camelContext = main.getOrCreateCamelContext();
+        camelContext.addRoutes(new MyRouteBuilder());
         main.run(args);
     }
 
